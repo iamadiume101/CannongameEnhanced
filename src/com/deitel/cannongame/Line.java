deb4c72ee7a533705ecf665487f7137ffcb90b4a
Ikem <iamadiume101@gmail.com>
2015-02-27 08:19:25 -0500

    first commit

---
+++ src/com/deitel/cannongame/Line.java
@@ -0,0 +1,12 @@
+
+//Line.java
+//Class Line represents a line with two endpoints
+package com.deitel.cannongame;
+
+import android.graphics.Point;
+
+public class Line {
+
+	public Point start = new Point();
+	public Point end = new Point();
+}
